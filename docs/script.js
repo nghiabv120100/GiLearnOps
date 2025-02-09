7898c9d30f37a56f1ff554c3ffc8bbc6fc74567a
NghiaBui <nghiabv120100@gmail.com>
2025-02-09 19:39:22 +0700

    Translate to VIetnamese

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Em chắc chứ?",
+    "Thật sự chắc chắn không??",
+    "Em có chắc 100% không?",
+    "Bé ơi, làm ơn mà...",
+    "Em suy nghĩ lại đi nhé!",
+    "Nếu em từ chối, anh sẽ buồn lắm đó...",
+    "Anh sẽ rất buồn luôn...",
+    "Anh sẽ buồn lắắắắm luôn đó...",
+    "Thôi được rồi, anh ngừng hỏi nữa...",
+    "Đùa thôi mà, đồng ý đi nhaa! ❤️"
 ];
 
 let messageIndex = 0;
